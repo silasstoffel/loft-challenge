6f9411cc0814cc8fd9f969b1f22817525e9a8cdd
Silas Stoffel <silasstofel@gmail.com>
2022-01-25 10:27:28 -0300

    chore: updated script config setup

--- bin/config-env.sh
+++ bin/config-env.sh
@@ -3,20 +3,20 @@
 echo "";
 
 echo "Instalação de dependencias [start]";
-docker container exec app-php composer install
+docker container exec loft-php composer install
 echo "Instalação de dependencias [finish]";
 
 echo "";
 
 echo "Aplicação de Permissões [start]";
-docker container exec app-php chown -Rf www-data:www-data ./storage
+docker container exec loft-php chown -Rf www-data:www-data ./storage
 echo "Aplicação de Permissões  [finish]";
 
 
 echo "";
 
 #echo "Executando Migrations [start]";
-#docker container exec app-php php artisan migrate --seed
+#docker container exec loft-php php artisan migrate --seed
 #echo "Executando Migrations [finish]";
 
 echo ""
